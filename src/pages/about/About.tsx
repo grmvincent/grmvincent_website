27720d0856246d7dd8d7ab074982fdee03c8beca
grmvincent <grmvincent.contact@gmail.com>
2023-11-07 16:31:44 +0100

    add favicon, titles, description

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function About() {
+    useEffect(() => {
+        document.title = 'grmvincent | about';
+    }, []);
+
     return (
         <div>
             <h1>About</h1>
--- src/pages/nomatch/NoMatch.tsx
+++ src/pages/nomatch/NoMatch.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function NoMatch() {
+    useEffect(() => {
+        document.title = 'grmvincent | no match';
+    }, []);
+
     return (
         <div>
             <h1>No Match !</h1>
